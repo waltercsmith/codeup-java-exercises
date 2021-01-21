ec9b49e6b8ce7873a65bdeb404adc28fc30fbace
Walter Smith <walter.c.smith01@gmail.com>
2021-01-21 15:21:29 -0600

    Completed Exercises

--- src/syntax/HelloWorld.java
+++ src/syntax/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 //        System.out.print("World");
 //
 //
-////       Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+//       Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
 //        int myFavoriteNumber = 308;
 //       System.out.println(myFavoriteNumber);
 ////       Create a String variable named myString and assign a string value to it, then print the variable out to the console.
@@ -19,21 +19,32 @@ public class HelloWorld {
 //       char a = myString.charAt(0);
 //       System.out.println(a);
 //       // Change your code to assign the value 3.14159 to myString
-////       myString = 3.14159; Does not work
+////       myString = 3.14159; Does not work but add double quotes to fix
 ////       Declare an long variable named myNumber, but do not assign anything to it
 ////       double myNumber;
 ////       print out myNumber to the console. What happens?
 ////       System.out.println(myNumber); // receive a prompt that variable myNumber has not been initialized
 ////       Change your code to assign the value 3.14 to myNumber. What do you notice
+
 ////        myNumber = 3.14; //Get red line over my assignment & error message that states that the required type is long but double was provided
+
 ////       Change your code to assign the value 123 to myNumber
+
 ////       myNumber = 123L;
 ////       myNumber = 123;
 ////       long myNumber;
+
+
 ////       myNumber = 3.14;
 //        //3.14 is a final constant in java
-////       float myNumber;
-////       myNumber = 3.14; //doesn't work
+
+
+//       float myNumber;
+
+
+//       float myNumber = 3.14f; //doesn't work but put f on the end
+
+
 ////       double myNumber = 3.14; // change to double works
 //
 //       int x = 5;
@@ -53,6 +64,9 @@ public class HelloWorld {
 
 //       int three = (int) "three"; // can not cast string to an int
 
+
+
+
 //       int x = 4;
 //       x = x + 5;
 //        x += 5; //shorthand assignment operators
@@ -60,15 +74,17 @@ public class HelloWorld {
 //       int x = 3;
 //       int y = 4;
 //       y = y * x;
-//       y *= x; //shorthand assignent operators
+//       y *= x; //shorthand assignment operators
 //
-//       int x = 10;
-//       int y = 2;
-//       x = x / y;
-//       y = y - x;
+       int x = 10;
+       int y = 2;
+       x = x / y;
+       y = y - x;
 //
-//       x /= y;
-//       y -= x;
+       System.out.println(x);
+       System.out.println(y);
+       int bigNumber = Integer.MAX_VALUE;
+       System.out.println(bigNumber);
 
        long number = (byte) 200;
 //        byte anothernumber = (long) 23434343; //not happening
