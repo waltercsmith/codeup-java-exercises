5c6945a1862b043a703cbdeb12df1479282d1135
Walter Smith <walter.c.smith01@gmail.com>
2021-01-22 11:53:15 -0600

    Copied Copy this code into your main method: double pi = 3.14159

--- src/syntax/ConsoleExercises.java
+++ src/syntax/ConsoleExercises.java
@@ -1,8 +1,13 @@
 package syntax;
 
 public class ConsoleExercises {
-    //Copy this code into your main method: double pi = 3.14159;
+
     public static void main(String[] args){
+        //Copy this code into your main method: double pi = 3.14159;
+
+    double pi = 3.14159;
+
+
 
     }
 
