bf114d632fe8e3fdba79a0dda08ab321f0724e4c
Walter Smith <walter.c.smith01@gmail.com>
2021-01-22 12:02:11 -0600

    Wrote some code to print: The value of pi is approximately 3.14

--- src/syntax/ConsoleExercises.java
+++ src/syntax/ConsoleExercises.java
@@ -7,6 +7,17 @@ public class ConsoleExercises {
 
     double pi = 3.14159;
 
+        //Write some Java code that uses the variable pi to output the following:
+        //The value of pi is approximately 3.14.
+
+        double shortValueOfPI = 3.14;
+
+        //Created a double data type with a value of 3.14
+
+
+        System.out.printf("The value of pi is: " + shortValueOfPI);
+
+
 
 
     }
