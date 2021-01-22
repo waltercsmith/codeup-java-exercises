724747f9edd407c7c5565dda3173d94a2387bc00
Walter Smith <walter.c.smith01@gmail.com>
2021-01-22 12:25:48 -0600

    Prompts a user to enter a integer and store that value in an int variable using the nextInt method

--- src/syntax/ConsoleExercises.java
+++ src/syntax/ConsoleExercises.java
@@ -1,4 +1,7 @@
+
 package syntax;
+
+import java.util.Scanner;
 
 public class ConsoleExercises {
 
@@ -15,9 +18,16 @@ public class ConsoleExercises {
         //Created a double data type with a value of 3.14
 
 
-//        System.out.printf("The value of pi is approximately: " + shortValueOfPI);
-        System.out.printf("The value of pi is approximately: %.2f", pi);
+        //System.out.printf("The value of pi is approximately: " + shortValueOfPI);
+        System.out.printf("The value of pi is approximately: %.2f%n", pi);
 
+        /*Prompt a user to enter a integer and store that value in an int variable using the
+        nextInt method.*/
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        Integer userInt = scanner.nextInt();
+        System.out.println("You entered: --> \"" + userInt + "\" <--");
 
 
 
