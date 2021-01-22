478621d948d6108b6b47c1b51b948eb98622bec7
Walter Smith <walter.c.smith01@gmail.com>
2021-01-22 14:53:30 -0600

    Display the area and perimeter of that classroom

--- src/syntax/ConsoleExercises.java
+++ src/syntax/ConsoleExercises.java
@@ -13,13 +13,9 @@ public class ConsoleExercises {
         //Write some Java code that uses the variable pi to output the following:
         //The value of pi is approximately 3.14.
 
-        double shortValueOfPI = 3.14;
-
-        //Created a double data type with a value of 3.14
 
 
-        //System.out.printf("The value of pi is approximately: " + shortValueOfPI);
-//        System.out.printf("The value of pi is approximately: %.2f%n", pi);
+//        System.out.printf("The value of pi is approximately: %.2f.%n", pi);
 
         /*Prompt a user to enter a integer and store that value in an int variable using the
         nextInt method.*/
@@ -27,7 +23,7 @@ public class ConsoleExercises {
         Scanner scanner = new Scanner(System.in);
 //        System.out.println("Enter something: ");
 //        Integer userInt = scanner.nextInt();
-//        System.out.println("You entered: --> \"" + userInt + "\" <--");
+//        System.out.println("You entered: " + userInt);
 
         /*When you input something that is not an integer then the console
         displays an "Exception in thread" message
@@ -36,12 +32,12 @@ public class ConsoleExercises {
         /*Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the        console, each on a newline.*/
 
 //        System.out.println("Enter 3 words");
-//        String word1 = scanner.nextLine();
-//        String word2 = scanner.nextLine();
-//        String word3 = scanner.nextLine();
-//        System.out.println("Here are the three words: --> \"" + word1 + "\" <--");
-//        System.out.println("Here are the three words: --> \"" + word2 + "\" <--");
-//        System.out.println("Here are the three words: --> \"" + word3 + "\" <--");
+//        String word1 = scanner.next();
+//        String word2 = scanner.next();
+//        String word3 = scanner.next();
+//        System.out.println("Here are the three words: " + word1);
+//        System.out.println("Here are the three words: " + word2);
+//        System.out.println("Here are the three words: \"" + word3);
         //Entering less than three words will result in an empty quote
 
         //Prompt a user to enter a sentence,
@@ -60,20 +56,20 @@ public class ConsoleExercises {
 
 
         //Prompt the user to enter values of length and width of a classroom at Codeup
-        System.out.println("Enter the length and width of a classroom");
-
-        //Use the nextLine method to get user input and parse the resulting string to a numeric type.
-        String userLengthString = scanner.nextLine();
-        String userWidthString = scanner.nextLine();
-        double userLengthDouble = Double.parseDouble(userLengthString);
-        double userWidthDouble = Double.parseDouble(userWidthString);
-        System.out.printf("The length is: "+ userLengthDouble + "%n" +"The width is: " + userWidthDouble + "%n");
-
-        //Display the area and perimeter of that classroom.
-        Double recArea = userLengthDouble * userWidthDouble;
-        Double recPerimeter = (userLengthDouble + 2) + (userWidthDouble + 2);
-
-        System.out.printf("The area is: "+ recArea + "%n" +"The perimeter is: " + recPerimeter);
+//        System.out.println("Enter the length and width of a classroom");
+//
+//        //Use the nextLine method to get user input and parse the resulting string to a numeric type.
+//        String userLengthString = scanner.nextLine();
+//        String userWidthString = scanner.nextLine();
+//        double userLengthDouble = Double.parseDouble(userLengthString);
+//        double userWidthDouble = Double.parseDouble(userWidthString);
+//        System.out.printf("The length is: "+ userLengthDouble + "%n" +"The width is: " + userWidthDouble + "%n");
+//
+//        //Display the area and perimeter of that classroom.
+//        Double recArea = userLengthDouble * userWidthDouble;
+//        Double recPerimeter = (userLengthDouble + 2) + (userWidthDouble + 2);
+//
+//        System.out.printf("The area is: "+ recArea + "%n" +"The perimeter is: " + recPerimeter);
 
 
     }
