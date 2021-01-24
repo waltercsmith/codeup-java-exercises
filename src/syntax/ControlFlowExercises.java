bcf69449309ac11431db95a0e1ccba56bd6c997f
Walter Smith <walter.c.smith01@gmail.com>
2021-01-23 19:24:30 -0600

    Created an integer, then while loop that runs so long as integer variable is less than or equal to 15.Each loop iteration, outputthe current value of the variable then increment by 1

--- src/syntax/ControlFlowExercises.java
+++ src/syntax/ControlFlowExercises.java
@@ -8,13 +8,24 @@ public class ControlFlowExercises {
         //Loop Basic:
         //While
         //Create an integer variable i with a value of 5
-        int i = 5;
+        //int i = 5;
 
         //Create a while loop that runs so long as i is less than or equal to 15
-        while(i <= 15){
-            System.out.println("i is " + i);
-            i++;
+//        while(i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
 
-        }
-    }
-}
+
+            //Do While
+        /* Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a             new line*/
+            int i = 0;
+            do {
+                System.out.println("You will see this despite the condition!");
+            }while (i <= 100);
+                    System.out.println("i is " + i);
+                i++;
+                }
+            }
+
+
