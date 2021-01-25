42d71b3c35ed5dab62aeb32581afe5160b90e76c
Walter Smith <walter.c.smith01@gmail.com>
2021-01-25 11:59:22 -0600

    Created a String variable that contains

--- src/syntax/StringExercise.java
+++ src/syntax/StringExercise.java
@@ -3,4 +3,32 @@ package syntax;
 public class StringExercise {
 
 
+    public static void main(String[] args) {
+
+
+
+    /*For each of the following output examples: create a String variable that contains
+    the desired output and print it out to the console, you can do this with only one
+     String variable and one print statement for each output example*/
+
+
+        String stringOne = "We don't need no education";
+        System.out.println(stringOne);
+
+        String stringTwo = "We don't need no thought control";
+        System.out.println(stringTwo);
+
+        String stringThree = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(stringThree);
+
+        String stringFour = "In windows, the main drive is usually C:\\";
+        System.out.println(stringFour);
+
+        String stringFive = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(stringFive);
+
+
+    }
+
 }
