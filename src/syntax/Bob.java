03fd2bee7d318fd2548458e82abf286d78a846cd
Walter Smith <walter.c.smith01@gmail.com>
2021-01-25 14:40:35 -0600

    Wrote the Java code necessary so that a user of your command line application can have a conversation with Bob

--- src/syntax/Bob.java
+++ src/syntax/Bob.java
@@ -15,8 +15,19 @@ public class Bob {
     then the console will print Bob's reply: "Sure"*/
     if (question.endsWith("?")){
         System.out.println("Bob's reply: \"Sure\"");
-    }
 
+
+     /*If the variable question contains an exclamation mark at the end of the string
+    then the console will print Bob's reply: "Whoa, chill out!' if you yell at him"*/
+
+    }else if (question.endsWith("!")){
+        System.out.println("Bob's reply: \"Whoa, chill out!\"");
+    }else if (question.endsWith("")){
+        System.out.println("Bob's reply: \"Whatever\"");
+    }
+    else {
+        System.out.println("Whatever");
+    }
 
     }
 }
