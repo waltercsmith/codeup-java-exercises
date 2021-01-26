cc991f56ef17c6a2364b8cf41423946123599b55
Walter Smith <walter.c.smith01@gmail.com>
2021-01-26 12:19:27 -0600

    Methods Exercises: Created four separate methods that multiply, add, subtract, & divide two numbers

--- src/syntax/Bob.java
+++ src/syntax/Bob.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args){
-        System.out.println("Ask Bob a question, please");
+//        System.out.println("Ask Bob a question, please");
 
 
         Scanner sc = new Scanner(System.in);
@@ -13,21 +13,28 @@ public class Bob {
 
     /*If the variable question contains a question mark at the end of the string
     then the console will print Bob's reply: "Sure"*/
-    if (question.endsWith("?")){
-        System.out.println("Bob's reply: \"Sure\"");
+//    if (question.endsWith("?")){
+//        System.out.println("Bob's reply: \"Sure\"");
 
 
      /*If the variable question contains an exclamation mark at the end of the string
     then the console will print Bob's reply: "Whoa, chill out!' if you yell at him"*/
 
-    }else if (question.endsWith("!")){
-        System.out.println("Bob's reply: \"Whoa, chill out!\"");
-    }else if (question.endsWith("")){
-        System.out.println("Bob's reply: \"Whatever\"");
-    }
-    else {
-        System.out.println("Whatever");
-    }
+//    }else if (question.endsWith("!")){
+//        System.out.println("Bob's reply: \"Whoa, chill out!\"");
+//    }else if (question.endsWith("")){
+//        System.out.println("Bob's reply: \"Whatever\"");
+//    }
+//    else {
+//        System.out.println("Whatever");
+//    }
 
+//        int x = 5;
+//                System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
+
+//        int x = 5 * 4 % 3;
+//        System.out.println(x);
+        String string = "Hey";
+        System.out.println(string   + 6);
     }
 }
--- src/syntax/MethodsExercises.java
+++ src/syntax/MethodsExercises.java
@@ -1,4 +1,27 @@
 package syntax;
 
 public class MethodsExercises {
+    //Create four separate methods. Each will perform an arithmetic operation:
+    //Addition
+    public static int addNumbers(int addend1, int addend2){
+            return addend1 + addend2;
+    }
+
+    //Substraction
+    public static int subtractNumbers(int minuend, int subtrahend){
+            return minuend - subtrahend;
+    }
+
+    //Multiplication
+    public static int multiplyNumbers(int factor1, int factor2){
+            return factor1 * factor2;
+    }
+
+    //Division
+    public  static int divideNumbers( int dividend, int divisor){
+            return dividend / divisor
+    }
+    public static void main(String [] args){
+
+    }
 }
