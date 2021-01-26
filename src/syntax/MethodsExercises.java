b3baba0bda591f32e288f0082670cb5006264f32
Walter Smith <walter.c.smith01@gmail.com>
2021-01-26 15:57:18 -0600

    Working on creating a method that validates the user input is within a certain range of numbers

--- src/syntax/MethodsExercises.java
+++ src/syntax/MethodsExercises.java
@@ -1,5 +1,5 @@
 package syntax;
-
+import java.util.Scanner;
 public class MethodsExercises {
     //Create four separate methods. Each will perform an arithmetic operation:
     //Addition
@@ -26,18 +26,59 @@ public class MethodsExercises {
     public static int modulusOfNumbers(int num1, int num2){
             return num1 % num2;
     }
+
+    //Create a method that validates that user input is in a certain range
+    public static int getInteger(int min, int max){
+        Scanner input = new Scanner(System.in);
+        Integer userInput = input.nextInt();
+
+
+            if(userInput >= min & userInput <= max ){
+                System.out.println("All done");
+            }
+
+            return userInput;
+
+    }
+
     public static void main(String [] args){
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number between 1 & 10, please.");
+        String userInput = input.nextLine();
+        System.out.println("You entered the number: " + userInput);
+        int range = MethodsExercises.getInteger(1,10);
+        System.out.println(range + " is within the number range");
+
+
         //Test your methods and verify the output
-        int sum = MethodsExercises.addNumbers(2,2);
-        System.out.println("2 + 2 = " + sum);
-        int difference = MethodsExercises.subtractNumbers(9,3);
-        System.out.println("9 - 3 = " + difference);
-        int product = MethodsExercises.multiplyNumbers(2,3);
-        System.out.println("2 x 3 = " + product);
-        int quotient = MethodsExercises.divideNumbers(12,6);
-        System.out.println("12 / 6 = " + quotient);
-        int modulus = MethodsExercises.modulusOfNumbers(10,3);
-        System.out.println("The remainder of 10 divided by 3 is: " + modulus);
+//        int sum = MethodsExercises.addNumbers(2,2);
+//        System.out.println("2 + 2 = " + sum);
+//        int difference = MethodsExercises.subtractNumbers(9,3);
+//        System.out.println("9 - 3 = " + difference);
+//        int product = MethodsExercises.multiplyNumbers(2,3);
+//        System.out.println("2 x 3 = " + product);
+//        int quotient = MethodsExercises.divideNumbers(12,6);
+//        System.out.println("12 / 6 = " + quotient);
+//        int modulus = MethodsExercises.modulusOfNumbers(10,3);
+//        System.out.println("The remainder of 10 divided by 3 is: " + modulus);
+
+//        //Create a method that validates that user input is in a certain range
+//        public static int getInteger(int min, int max){
+//            Scanner input = new Scanner(System.in);
+//            System.out.println("Enter a number between 1 & 10, please.");
+//            int userInput = input.nextInt();
+//
+//            if(userInput >= min & userInput <= max ){
+//                System.out.println("All done");
+//            }
+//
+//            return userInput;
+
+
+
+//
+//        }
+
     }
 }
