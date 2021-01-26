7f5534d7679aa3a79a33810d77e40a42c6dc8873
Walter Smith <walter.c.smith01@gmail.com>
2021-01-26 13:40:26 -0600

    Added a modulus method that finds the modulus of two numbers

--- src/syntax/MethodsExercises.java
+++ src/syntax/MethodsExercises.java
@@ -21,6 +21,11 @@ public class MethodsExercises {
     public  static int divideNumbers( int dividend, int divisor){
             return dividend / divisor;
     }
+
+    //Modulus method that finds the modulus of two numbers
+    public static int modulusOfNumbers(int num1, int num2){
+            return num1 % num2;
+    }
     public static void main(String [] args){
 
         //Test your methods and verify the output
@@ -32,5 +37,7 @@ public class MethodsExercises {
         System.out.println("2 x 3 = " + product);
         int quotient = MethodsExercises.divideNumbers(12,6);
         System.out.println("12 / 6 = " + quotient);
+        int modulus = MethodsExercises.modulusOfNumbers(10,3);
+        System.out.println("The remainder of 10 divided by 3 is: " + modulus);
     }
 }
