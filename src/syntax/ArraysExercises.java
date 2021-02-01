a9a707156ec72a10bb173ac85411ce64a0f7a249
Walter Smith <walter.c.smith01@gmail.com>
2021-02-01 16:35:37 -0600

    Exercise Completed

--- src/syntax/ArraysExercises.java
+++ src/syntax/ArraysExercises.java
@@ -1,11 +1,23 @@
 package syntax;
 
+import java.util.Arrays;
+
 public class ArraysExercises {
 
 
 
 
     public static void main(String[] args){
+        //What happens when you run the following code?
+        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers);
+
+        //The console displays a bracket some characters and numbers
+
+        //Why is Arrays.toString necessary
+
+        System.out.println(Arrays.toString(numbers));
+
 
     }
 }
