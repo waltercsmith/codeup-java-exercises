1f7c799224c79a057bd8c1ba0ccce63ab843a2b1
Walter Smith <walter.c.smith01@gmail.com>
2021-02-02 11:52:39 -0600

    Created an array that holds three Person objects

--- src/syntax/ArraysExercises.java
+++ src/syntax/ArraysExercises.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
 public class ArraysExercises {
 
 
+    public static void addPerson(){
 
+    }
 
     public static void main(String[] args){
         //What happens when you run the following code?
@@ -21,15 +23,25 @@ public class ArraysExercises {
 
         System.out.println(Arrays.toString(numbers));
 
-        Person person1 = new Person("Walter");
-        Person person2 = new Person("Chris");
-        Person person3 = new Person("Dan");
+        Person people[] = new Person[3];
+        //Created an array that holds three Person objects
 
-        Person people[] = {person1,person2,person3};
+
+//        Person person1 = new Person("Walter");
+//        Person person2 = new Person("Chris");
+//        Person person3 = new Person("Dan");
+
+//        Person people[] = {person1,person2,person3};
+
+//        for (int i = 0; i < people.length; i++){
+//            System.out.println(people);
+//        }
 
 //        for (int peoples : people){
 //            System.out.println();
 //        }
+
+
 
 
     }
