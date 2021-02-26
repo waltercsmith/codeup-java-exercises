137e3f45632cc75e71e9610b8d4e38cc24966865
Walter Smith <walter.c.smith01@gmail.com>
2021-02-26 15:00:16 -0500

    Created a class called circle that provides methods that returns the radius, area, and circumference of a circle.

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,2 +1,36 @@
-package shapes;public class CircleApp {
-}
+package shapes;
+
+import util.Input;
+import java.util.Scanner;
+
+public class CircleApp {
+
+    public double getDouble () {
+        double number;
+
+//        try{ number=Double.valueOf(getString());
+//            System.out.printf("please enter a double: %n");
+//            return number;
+//        }catch (NumberFormatException e){
+//            System.err.println("array out of bounds");;
+//            return getDouble();
+//
+//        }
+
+    }
+    public static void main(String[] args) {
+
+
+        Input input=new Input();
+//        double radius = input.getDouble();
+
+
+        Circle circle = new Circle(radius);
+
+        System.out.println("The Area is : " + circle.getArea());
+        System.out.println("The Circumference is : " + circle.getCircumference());
+
+    }
+
+    }
+
